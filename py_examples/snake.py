1777bb567b5a0d59a0892ab62ffbaf3d2439f6b6
acup1 <georgiyegoriy@gmail.com>
2025-11-04 04:20:28 +0300

    snake py example

--- py_examples/snake.py
+++ py_examples/snake.py
@@ -1,31 +1,86 @@
 import termutils as tu
+from math import cos, sin, pi
 
 
 class snake_segment(tu.Window):
-    def __init__(self):
+    def __init__(self, size):
         super().__init__(0)
-        self.x=10
-        self.y=10
-        self.width=5
-        self.height=3
+        self.width, self.height = size
+        self.x, self.y = 0, 0
+        self.always_on_screen = 0
 
 
 class snake:
-    tail=[]
+    tail = []
+    direction = 0
+    pre_direction = 0
+    size = (5, 3)
+
     def __init__(self):
         pass
 
-        
+    def __add__(self, inc):
+        for _ in range(inc):
+            seg = snake_segment(self.size)
+            if len(self.tail):
+                seg.x = int(
+                    self.tail[-1].x + cos(self.direction * pi / 2) * self.size[0]
+                )
+                seg.y = int(
+                    self.tail[-1].y - sin(self.direction * pi / 2) * self.size[1]
+                )
+            self.tail.append(seg)
+        return self
+
+    def move(self):
+        if len(self.tail) > 1:
+            self.direction = self.pre_direction
+            seg = self.tail.pop(0)
+            seg.x = int(self.tail[-1].x + cos(self.direction * pi / 2) * self.size[0])
+            seg.y = int(self.tail[-1].y - sin(self.direction * pi / 2) * self.size[1])
+            self.tail.append(seg)
+
+    def turn(self, direction):
+        if abs(self.direction - direction) != 2:
+            self.pre_direction = direction
+
+    def __call__(self, action, *args):
+        if action == "move":
+            self.move()
+        elif action == "turn":
+            self.turn(args[0])
+
 
 tu.init()
 tu.cursset(0)
 
-a=snake_segment()
+s = snake()
+s += 3
 
-tu.posprint(0,0,'')
+frame = 0
+
+tu.posprint(0, 0, "")
 tu.render_windows()
-tu.refresh()
+c = chr(tu.getch(2, 1))
 
-while tu.getch(100,0)!=ord('q'):tu.refresh()
+while c != "q":
+    if c == "d":
+        s("turn", 0)
+    elif c == "w":
+        s("turn", 1)
+    elif c == "a":
+        s("turn", 2)
+    elif c == "s":
+        s("turn", 3)
+
+    if frame % 50 == 0:
+        s("move")
+        frame = 0
+    tu.clear()
+    tu.render_windows()
+
+    frame += 1
+    c = chr(tu.getch(2, 1))
+
 
 tu.restore()
